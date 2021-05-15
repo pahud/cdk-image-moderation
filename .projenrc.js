4aa68583f6914fbd7e1781bc7ecbd972c0212cc7
Automation <github-actions@github.com>
2021-05-15 00:25:49 +0000

    chore: self mutation

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,5 @@
-const { AwsCdkConstructLibrary } = require('projen');
 const { Automation } = require('projen-automate-it');
+const { AwsCdkConstructLibrary } = require('projen');
 const { Mergify } = require('projen/lib/github');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
